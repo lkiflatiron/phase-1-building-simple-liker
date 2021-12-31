84317aa1510c6096fd78e700830418d90372a107
Louise Iyengar <louise.iyengar@flatironschool.com>
2021-12-30 19:30:33 -0500

    Done

--- main.js
+++ main.js
@@ -3,7 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.getElementsByClassName('like-glyph')
 
+
+for (const heart of hearts) {
+  heart.addEventListener("click", processLike)
+}
+
+function processLike(e) {
+  mimicServerCall()
+  .then((res) => {toggleHeart(e)})
+  .catch(rej => {hideModal(rej)})
+}
+
+function toggleHeart(e) {
+  if (e.target.textContent === EMPTY_HEART) {
+    e.target.textContent = FULL_HEART
+    e.target.classList.add('activated-heart')
+  } else {
+    e.target.textContent = EMPTY_HEART
+    e.target.classList.remove('activated-heart')
+  }
+}
+
+function hideModal(rej) {
+  const modal = document.getElementById('modal')
+  const messageArea = document.getElementById('modal-message')
+  messageArea.textContent = rej
+  modal.classList.remove('hidden')
+  setTimeout(()=> modal.classList.add("hidden"),3000)
+}
 
 
 
@@ -14,7 +43,7 @@ const FULL_HEART = '♥'
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+      let isRandomFailure = Math.random() < .1
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
